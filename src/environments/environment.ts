0c488cc3428f6713d1ea7b9c80e07ba13ae35552
vajin1125 <31552910+vajin1125@users.noreply.github.com>
2023-02-02 11:00:17 +0200

    completed project

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD7KfE6Q_tYEX0PLIEy0SX1gtgFxduaDyI",
+    authDomain: "sample-project-7816d.firebaseapp.com",
+    databaseURL: "https://sample-project-7816d-default-rtdb.firebaseio.com",
+    projectId: "sample-project-7816d",
+    storageBucket: "sample-project-7816d.appspot.com",
+    messagingSenderId: "698891488900",
+    appId: "1:698891488900:web:b7ff27c339029821a03cd1",
+    measurementId: "G-XDP4WNLJLN"
+  }
+}
